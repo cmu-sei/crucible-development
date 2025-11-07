03de52150b935305af7ec91dddc122fbc8c2ff44
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2025-11-07 21:43:06 +0000

    recreate minikube cluster if mount changes

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-minikube start --mount-string="/mnt/data/terraform/root:/terraform/root" --embed-certs
+start_minikube() {
+    local output_file=$1
+    local cmd="minikube start --mount-string=\"/mnt/data/terraform:/mnt/data/terraform\" --embed-certs"
+
+    if [ -n "$output_file" ]; then
+        # use script to preserve colorized terminal output while capturing to a file
+        script -q -e -c "$cmd" "$output_file"
+    else
+        eval "$cmd"
+    fi
+}
+
+TMPFILE=$(mktemp)
+trap "rm -f $TMPFILE" EXIT
+
+if ! start_minikube "$TMPFILE"; then
+    if grep -q "GUEST_MOUNT_CONFLICT" "$TMPFILE"; then
+        echo "⚠️   Detected mount conflict — recreating minikube cluster ..."
+        minikube delete
+        start_minikube
+    fi
+fi
