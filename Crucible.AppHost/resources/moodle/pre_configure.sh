eda44a3b5806ed053f12f88b01822bda74b74072
Adam Welle <arwelle@sei.cmu.edu>
2025-11-13 14:57:25 -0500

    changes chmod of scripts

--- Crucible.AppHost/resources/moodle/pre_configure.sh
+++ Crucible.AppHost/resources/moodle/pre_configure.sh
@@ -1,6 +1,12 @@
+# TODO: adjust permissions
+
+echo removing old moodle core files
+rm -rf /var/www/html/theme
+rm -rf /var/www/html/lib
+rm -rf /var/www/html/admin/cli
 
 # TODO: make sure that the dev container has the mounts set to 777 first
-echo copying files
+echo copying moodle core files for debugging
 cp -r /moodle/theme /var/www/html/
 cp -r /moodle/lib /var/www/html/
 cp -r /moodle/admin/cli /var/www/html/admin/
