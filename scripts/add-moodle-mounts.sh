b1f9037ede7eb9d45d79b934fc458c1873df144a
Adam Welle <arwelle@sei.cmu.edu>
2025-11-13 20:40:31 -0500

    moves moodle-core files into directory created by scripts called by postcreat.sh

---
+++ scripts/add-moodle-mounts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p /mnt/data/crucible/moodle/moodle-core/
+mkdir -p /mnt/data/crucible/moodle/moodle-core/theme
+mkdir -p /mnt/data/crucible/moodle/moodle-core/lib
+mkdir -p /mnt/data/crucible/moodle/moodle-core/admin/cli
+chmod 777 /mnt/data/crucible/moodle/moodle-core/
+chmod 777 /mnt/data/crucible/moodle/moodle-core/theme
+chmod 777 /mnt/data/crucible/moodle/moodle-core/lib
+chmod 777 /mnt/data/crucible/moodle/moodle-core/admin/cli
+
