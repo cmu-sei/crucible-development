8896f262d27b38fbfb412fd740bcbd5dcda07e82
Jarrett Booz <jabooz@sei.cmu.edu>
2025-12-11 17:25:17 +0000

    Adds dependent helm repos

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -39,6 +39,8 @@ declare -A HELM_REPOS=(
   [ingress-nginx]="https://kubernetes.github.io/ingress-nginx"
   [kvaps]="https://kvaps.github.io/charts"
   [selfhosters]="https://self-hosters-by-night.github.io/helm-charts"
+  [runix]="https://helm.runix.net"
+  [grafana]="https://grafana.github.io/helm-charts"
 )
 
 for name in "${!HELM_REPOS[@]}"; do
